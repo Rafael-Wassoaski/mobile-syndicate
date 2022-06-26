967850d7edd6f1be1de2b6a0971721f489033f3c
Rafael Wassoaski <meireles200@hotmail.com>
2022-06-26 18:30:34 -0300

    MOB_SYN 3 feat(main): #time 1h 30m Adding buttons and icons

--- app/src/main/java/com/midnight/sindicato/ui/MainActivity.java
+++ app/src/main/java/com/midnight/sindicato/ui/MainActivity.java
@@ -3,10 +3,13 @@ package com.midnight.sindicato.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -28,30 +31,36 @@ public class MainActivity extends AppCompatActivity {
         String message = intent.getStringExtra("Message");
 
         Log.d("Message", message);
+        Drawable img = getApplicationContext().getResources().getDrawable(R.drawable.ic_info);
+        img.setBounds( 0, 0, 256, 256 );
+        img.setColorFilter( 0xffff0000, PorterDuff.Mode.MULTIPLY );
 
         Button button1 = new Button(this);
         button1.setBackground(getDrawable(R.drawable.square));
+        button1.setCompoundDrawables(null, img, null , null);
+        button1.setPadding(0, 60, 0, 0);
+        button1.setCompoundDrawablePadding(-15);
+
         button1.setText("aaaa");
 
         addButtonToTable(button1);
 
         Button button2 = new Button(this);
         button2.setBackground(getDrawable(R.drawable.square));
-        Drawable img = getApplicationContext().getResources().getDrawable(R.drawable.ic_launcher_background);
-        img.setBounds( 0, 0, 60, 60 );
         button2.setCompoundDrawables(null, img, null , null);
         button2.setPadding(0, 60, 0, 0);
         button2.setCompoundDrawablePadding(-15);
-        button2.setWidth(10);
-        button2.setHeight(50);
         button2.setText("aaaa");
-
         addButtonToTable(button2);
 
         Button button3 = new Button(this);
         button3.setBackground(getDrawable(R.drawable.square));
-        button3.setText("aaaa");
+        button3.setCompoundDrawables(null, img, null , null);
+        button3.setPadding(0, 60, 0, 0);
 
+        button3.setCompoundDrawablePadding(-15);
+
+        button3.setText("aaaa");
         addButtonToTable(button3);
     }
 
@@ -61,8 +70,12 @@ public class MainActivity extends AppCompatActivity {
 
         if(tableRow.getChildCount() == 2){
             tableRow = new TableRow(this);
+            tableRow.setPadding(0, 0, 0, 50);
+            TableRow.LayoutParams tableRowLayout = new TableRow.LayoutParams();
+            tableRowLayout.rightMargin = 50;
+            button.setLayoutParams(tableRowLayout);
 
-            tableRow.setLayoutParams(new TableLayout.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.MATCH_PARENT));
+            tableRow.setLayoutParams(tableRowLayout);
             tableLayout.addView(tableRow);
         }
 
